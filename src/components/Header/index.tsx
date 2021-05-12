8fbd93892f792644ed7e7ed26bd7c0537e22a318
Marcelo Anselmo de Souza Filho <marcelofilho@mpf.mp.br>
2021-05-12 16:56:56 -0300

    🍺 Aê mlk  preula

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,48 @@
-import logoImg from '../../assets/logo.png'
-
-import { Container, Content } from './styles'
+import logoImg from "../../assets/logo.png";
+import { Container, Content } from "./styles";
 
 interface HeaderProps {
+  qualAmodalQueEstaAbertaAgora: string;
   onOpenNewTimeInputModal: () => void;
   onLaunchBreakModal: () => void;
   onExitTimeModal: () => void;
 }
 
-export function Header({onOpenNewTimeInputModal, onLaunchBreakModal, onExitTimeModal}: HeaderProps) {
+export function Header({
+  qualAmodalQueEstaAbertaAgora,
+  onOpenNewTimeInputModal,
+  onLaunchBreakModal,
+  onExitTimeModal,
+}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="logo" />
-        <button type="button" onClick={onOpenNewTimeInputModal}>
-          Entry time
-        </button>
-        <button type="button" onClick={onLaunchBreakModal}>
-          Launch break
-        </button>
-        <button type="button" onClick={onExitTimeModal}>
-          Exit time
-        </button>
+
+        {qualAmodalQueEstaAbertaAgora === "EntryTime" && (
+          <button type="button" onClick={onOpenNewTimeInputModal}>
+            Entry time
+          </button>
+        )}
+
+        {qualAmodalQueEstaAbertaAgora === "LaunchBreak" && (
+          <button type="button" onClick={onLaunchBreakModal}>
+            Launch break
+          </button>
+        )}
+
+        {qualAmodalQueEstaAbertaAgora === "ExitTime" && (
+          <button type="button" onClick={onExitTimeModal}>
+            Exit time
+          </button>
+        )}
+
+        {qualAmodalQueEstaAbertaAgora === "acabou" && (
+          <button disabled type="button" onClick={onExitTimeModal}>
+            Exit time
+          </button>
+        )}
       </Content>
     </Container>
-  )
+  );
 }
